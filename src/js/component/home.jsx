756fe3b787e64928e792e74159957d10728ca5ae
Alejandro <eadelgadom@gmail.com>
2022-04-30 13:59:51 +0000

    finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [selectedColor, setSelectedColor] = useState("red");
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="body1">
+			<h1 className="text-center mt-5">TRAFINC LIGTHS WITH REACT!</h1>
+			<div className="bodybath">
+				<div className="bath"></div>
+			</div>
+			<div className="body2">
+				<div className="box">
+					<div
+						onClick={() => setSelectedColor("red")}
+						className={
+							"ligth red" +
+							(selectedColor === "red" ? " glow" : " ")
+						}></div>
+					<div
+						onClick={() => setSelectedColor("yellow")}
+						className={
+							"ligth yellow" +
+							(selectedColor === "yellow" ? " glow" : " ")
+						}></div>
+					<div
+						onClick={() => setSelectedColor("green")}
+						className={
+							"ligth green" +
+							(selectedColor === "green" ? " glow" : " ")
+						}></div>
+				</div>
+			</div>
+			<p>Mady be persyjr in 4geeks Academy</p>
 		</div>
 	);
 };
